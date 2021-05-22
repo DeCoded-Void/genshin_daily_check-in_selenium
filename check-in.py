8b43521dcf9a984b3a01614d8abfd2e7a9c51c25
YozenPL <lyozenl@gmail.com>
2021-05-22 17:06:01 +0200

    Update check-in.py

--- check-in.py
+++ check-in.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-import time
-import pickle
+import time, winsound, pickle, ctypes
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
 
 options = FirefoxOptions()
-options.headless = False
-driver = webdriver.Firefox(options=options, executable_path = 'C:\geckodriver.exe')
+options.headless = True
+gecko_logpath = 'nul'
+driver = webdriver.Firefox(options = options, executable_path = 'C:\geckodriver.exe', service_log_path = gecko_logpath)
 
 def open_browser():
     driver.get('https://webstatic-sea.mihoyo.com'
@@ -24,6 +24,17 @@ def reload_browser():
     driver.get('https://webstatic-sea.mihoyo.com'
                '/ys/event/signin-sea/index.html?act_id=e202102251931481&lang=en-us')
 
+def check_logon():
+    submit = driver.find_element_by_xpath('//*[contains(@class, "---login")]')
+    submit.click()
+    elements = driver.find_elements_by_xpath('//*[contains(text(), "Log out")]')
+    if not elements:
+        print("You are not logged in!")
+        # Sound when logon failed
+        #winsound.PlaySound("error.wav", winsound.SND_ASYNC | winsound.SND_ALIAS)
+        # Popup when logon failed
+        ctypes.windll.user32.MessageBoxW(0, "You are not logged in!", "Genshin Impact daily check-in event!", 0)
+
 def execute_click():
     submit = driver.find_element_by_xpath('//*[contains(@class, "---active")]')
     submit.click()
@@ -34,13 +45,15 @@ def close_browser():
 if __name__ == '__main__':
     open_browser()
     #import_cookies()
-    time.sleep(60) #get some time to log in (60 seconds)
+    time.sleep(60) #get some time to log in
     get_cookies()
     reload_browser()
+    time.sleep(5)
+    check_logon()
     time.sleep(5)
     try:
         execute_click()
         time.sleep(5)
     except:
-        print("Already redemeed.")
+        print("Alreaady redemeed.")
     close_browser()
