d5f05ef088b944c746ab34b2ab5733af09668b94
YozenPL <lyozenl@gmail.com>
2021-05-22 17:18:11 +0200

    Update check-in.py

--- check-in.py
+++ check-in.py
@@ -56,5 +56,5 @@ if __name__ == '__main__':
         execute_click()
         time.sleep(5)
     except:
-        print("Alreaady redemeed.")
+        print("Already redemeed.")
     close_browser()
